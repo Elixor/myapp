2c92a1a7b78e447d20df1a704f404ad50763efa4
elixor <skarab.cz@seznam.cz>
2012-09-29 21:51:49 +0200

    first step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewProject::Application.config.secret_token = '67227042b376cc05f7eca24aa181513c8c7db49bfa5c51a93d1499dce434890c7006b4de52551ba788c0f58403441f8d8e2a1db639cce44ccdfba1194291c079'
