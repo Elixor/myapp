736ca8512d08e3e5076948368fcddc25775f6d86
elixor <skarab.cz@seznam.cz>
2012-09-30 18:43:39 +0200

    second step

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,16 @@ class WelcomeController < ApplicationController
   end
 
   def registration
+  	@user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
+
 end
