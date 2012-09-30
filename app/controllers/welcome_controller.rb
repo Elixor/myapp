a3ff3133549c487308191e61614faaa06051956a
David Horsák <david.horsak@nevico.eu>
2012-09-30 20:19:57 +0300

    Notes to welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,38 @@
+# encoding: UTF-8
+
+# COMMENT
+# 1. Kontrolery se pojmenovavaji v mnoznem cislo, napr. UsersController, ContactsController
+# 2. Nauc se pouzivat respond_to blok
+#
+#   respond_to do |format|
+#     format.html { kod nebo zadny kod }
+#   end
+#
+#   doc: http://api.rubyonrails.org/classes/ActionController/MimeResponds.html#method-i-respond_to
+#
+#   Mozna ti ted bude trochu unikat smysl, ale jakmile zacnes pridavat ajax, json, js apod. Bude se to hodit.
+#   Prihodil jsem priklad
+#
+#   3. uvazuj REST! Kazdy kotroler ma svou ulohu a stara se pouze o jeden model (v idealnim pripade)
+#     priklad. kontroller, ktery bude mit na starosti registrace bude RegistrationsController, 
+#     kontroller pro prihlasovani bude SessionsController apod.
+#
+#     Vubec nevadi, kdyz ma kontroler jenom jednu akci, dulezite je udrzet kontrolery prehledne, tzv. jsou "dry"
+#
+#   4. na radku 1. jsem pridal tzv. "magic comment". Rika ruby, pomoci jakeho kodovani ma tento soubor cist.
+#     kdyby tam nebyl, stacil byl jediny non-ASCII symbol a interpreter by tento soubor neprecetl. Doporucuju pouzivat,
+#     protoze jsme v cechach a pouzivame hacky carky, tzv. accents. Mimo to, mame ceskou klavesnici a i nedopatrenim
+#     (pouzitim klavesove zkratky) muzeme vlozit neviditelny znak, ktery je taky non-ASCII. UTF vi, ze je neviditelny,
+#     tak ho ignoruje, ASCII ho nezna a vyhodi chybu
+
 class WelcomeController < ApplicationController
   def index
+#   respond_to do |format|
+#     format.html
+#   end
   end
 
+  # viz poznamka o 3.
   def registration
   	@user = User.new
   end
@@ -13,6 +44,15 @@ class WelcomeController < ApplicationController
     else
       render "new"
     end
+    
+#   respond_to do |block|
+#     if @user.save
+#       format.html { redirect_to root_url, :notice => "Signed up!" }
+#     else
+#       format.html { render "new" }
+#     end
+#   end
+
   end
 
 end
