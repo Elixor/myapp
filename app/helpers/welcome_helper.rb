441eda659dd3dcd8c3a415f887b942d72940c5ac
David Horsák <david.horsak@nevico.eu>
2012-09-30 20:23:49 +0300

    Notes on welcome_helper.rb

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,12 @@
+# encoding: UTF-8
+
+# COMMENT
+# Stejne jako u kontoroleru - mnozne cislo ve jmene. Akorat bacha! jmeno unitr musi odpovidat i jmenu souboru.
+#
+# napr. uvnitr welcomes_helper.rb muze sidlit pouze module WelcomesHelper. Nic jineho!
+#
+# Ty magic commenty pridavej jenom tam, kde neco menis. V nove vygenerovanych souborech se nemusi bat o to, ze tam
+# vlozis neco, co tam nepatri.
+
 module WelcomeHelper
 end
